b2f4b5ee129c3e004975ed7e11b647273e54b5fb
Toluwani1017 <ayodelete@jacks.sfasu.edu>
2021-11-19 06:26:00 -0600

    changes

---
+++ GuitarShop_001/Mandolin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuitarShop_001
+{
+    class Mandolin: Instrument
+    {
+
+        public Mandolin(string serialNum, double price, MandolinSpec mandolinSpec): base(serialNum, price, mandolinSpec)
+        {
+
+        }
+    }
+}
